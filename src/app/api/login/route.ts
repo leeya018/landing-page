51aac0f2fb2297a932d4de8000050e3e350530cb
leeya018 <leeyahav018@gmail.com>
2025-03-14 18:57:30 +0200

    s

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,35 +2,6 @@
 
 import { addCred, Cred } from "../../../../database/creds";
 
-// export async function POST(request: Request) {
-//   try {
-//     const body = await request.json();
-//     const { email, password } = body;
-
-//     // Basic validation
-//     if (!email || !password) {
-//       return Response.json(
-//         { message: "Email and password are required" },
-//         { status: 400 }
-//       );
-//     }
-
-//     return Response.json({ message: "Login is good" }, { status: 200 });
-
-//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//   } catch (error: unknown) {
-//     return Response.json({ message: "Something went wrong" }, { status: 500 });
-//   }
-// }
-
-// // Optionally export other methods
-// export async function GET() {
-//   return Response.json(
-//     { message: "GET not supported on this endpoint" },
-//     { status: 405 }
-//   );
-// }
-
 interface LoginBody {
   email: string;
   password: string;
